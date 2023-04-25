b6fabc2e185f99fb7c5db13644a49a66c7917837
Aslanbek Astamirov <asmmmmir@gmail.com>
2023-04-25 23:07:21 +0500

    added 404 page

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,6 +3,7 @@ import Main from "./Main";
 import User from "./User";
 import Balance from "./Balance";
 import RouterLayout from "./RouterLayout";
+import NotFound from "./NotFound";
 
 const AppRouter = () => {
     return (
@@ -12,6 +13,7 @@ const AppRouter = () => {
             <Route path="/" element={<Main />} />
             <Route path="user" element={<User />} />
             <Route path="balance" element={<Balance />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
